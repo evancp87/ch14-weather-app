ba9585365e8846b299ed2616b0cc0c565417c2dd
Evan Parker <65721884+evancp87@users.noreply.github.com>
2023-05-08 16:30:27 +0100

    fleshed out drag api code, still not working

--- scripts/drag.js
+++ scripts/drag.js
@@ -1,52 +1,61 @@
 let dragged;
-const favItems = document.querySelectorAll(".favourite");
+const draggableItems = [...document.querySelectorAll(".favorite")];
 
-favItems.map((fav) => {
-  fav.setAttribute("draggable", true);
+const handleDragStart = (e) => {
+  console.log("dragstart");
+  e.dataTransfer.setData("text/plain", null);
+  e.dataTransfer.effectAllowed = "move";
+  e.dataTransfer.setData("special", "panda");
+  dragged = e.target;
+  dragged.style.opacity = "0.5";
+  dragged.style.scale = "1.2";
+};
 
-  fav.addEventListener("dragstart", handleDragStart);
+const handleDragEnd = () => {
+  dragged.style.opacity = "";
+  dragged.style.scale = "";
+  // dragged.classList.remove("")
+};
+const handleDragOver = (e) => {
+  e.preventDefault();
+  console.log("dragover");
+};
 
-  const handleDragStart = (e) => {
-    console.log("dragstart");
-    e.dataTransfer.setData("text/plain", null);
-    e.dataTransfer.setData("special", "panda");
-    dragged = e.target;
-    dragged.style.opacity = "0.5";
-    dragged.style.scale = "1.2";
-  };
+const handleDragEnter = (e) => {
+  const { target } = e;
+  target.classList.contains("dropzone") ? (target.style.border = "dashed") : "";
+};
 
-  fav.addEventListener("dragend", handleDragEnd);
+const handleDragLeave = (e) => {
+  target.classList.contains("dropzone") ? (target.style.border = "") : "dashed";
+};
 
-  const handleDragEnd = () => {
-    dragged.style.opacity = "";
-    dragged.style.scale = "";
-    // dragged.classList.remove("")
-  };
+const handleDrop = (e) => {
+  e.preventDefault();
+  e.stopPropagation();
 
-  fav.addEventListener("dragover", handleDragOver);
+  const { target } = e;
 
-  const handleDragOver = (e) => {
-    e.preventDefault();
-    console.log("dragover");
-  };
+  if (target.className === "dropzone") {
+    const dZone = target;
+    // const txt = dZone.textContent.trim();
+    console.log("data", e.dataTransfer.getData("special"));
+    // const number = txt[txt.length - 1];
+    dZone.style.border = "";
+    dragged.parentNode.removeChild(dragged);
+    dZone.appendChild(dragged);
+    // dragged.textContent = `Dropped in (${number})`;
+  }
+};
 
-  fav.addEventListener("dragleave", false, handleDragLeave);
-  fav.addEventListener("dragenter", handleDragEnter);
-
-  const handleDragEnter = (e) => {
-    const { target } = e;
-    target.classList.contains("dropzone")
-      ? (target.style.border = "dashed")
-      : "";
-  };
-
-  const handleDragLeave = (e) => {
-    target.classList.contains("dropzone")
-      ? (target.style.border = "")
-      : "dashed";
-  };
-
-  fav.addEventListener("drop", handleDrop);
-
-  const handleDrop = (e) => {};
-});
+export const loadDraggableItems = () => {
+  draggableItems.map((item) => {
+    item.setAttribute("draggable", true);
+    item.addEventListener("dragstart", handleDragStart);
+    item.addEventListener("dragend", handleDragEnd);
+    item.addEventListener("dragover", handleDragOver);
+    item.addEventListener("dragleave", false, handleDragLeave);
+    item.addEventListener("dragenter", handleDragEnter);
+    item.addEventListener("drop", handleDrop);
+  });
+};
