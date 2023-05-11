0e2c5fcadf5420ad5fdf6c50f50be97b2d4884ce
Evan Parker <65721884+evancp87@users.noreply.github.com>
2023-05-11 14:18:35 +0100

    helper functions added

--- scripts/helpers.js
+++ scripts/helpers.js
@@ -1,71 +1,78 @@
-// // example 2
-// function createWeatherCard(data) {
-//   const {
-//     timezone,
-//     current: {
-//       temp,
-//       weather: [{ main, icon }],
-//     },
-//   } = data;
+export function createWeatherCardEl(tag, attributes = [], text = "") {
+  const element = document.createElement(tag);
 
-//   const elements = [
-//     {
-//       type: "div",
-//       class: "card",
-//       children: [
-//         {
-//           type: "div",
-//           class: "card__text",
-//           children: [
-//             { type: "p", textContent: "Current" },
-//             { type: "h2", textContent: timezone },
-//             { type: "p", textContent: main },
-//           ],
-//         },
-//         {
-//           type: "img",
-//           class: "weather-icon",
-//           attributes: [
-//             {
-//               name: "src",
-//               value: `https://openweathermap.org/img/w/${icon}.png`,
-//             },
-//           ],
-//         },
-//         { type: "p", textContent: `${Math.round(temp - 273.15)}°C` },
-//       ],
-//     },
-//   ];
-
-//   const card = createElementFromObject(elements[0]);
-
-//   app.appendChild(card);
-// }
-
-export default function createWeatherCard({
-  type,
-  textContent,
-  class: className,
-  children = [],
-  attributes = [],
-}) {
-  const element = document.createElement(type);
-
-  if (textContent) {
-    element.textContent = textContent;
-  }
-
-  if (className) {
-    element.classList.add(className);
+  if (text !== "") {
+    const _text = document.createTextNode(text);
+    element.append(_text);
   }
 
   attributes.forEach(({ name, value }) => {
     element.setAttribute(name, value);
   });
 
-  children.forEach((child) => {
-    element.appendChild(createWeatherCard(child));
-  });
-
+  // document.getElementById(id).append(element);
   return element;
 }
+
+export const createWeatherCard = (domRef, elements) => {
+  const root = document.getElementById(domRef);
+
+  elements.forEach((element) => {
+    root.appendChild(element);
+  });
+};
+
+const weatherCardEls = [
+  createWeatherCardEl([{ name: "class", value: "card" }], "div", "Sunny"),
+  createWeatherCardEl([{ name: "class", value: "card" }], "div", "Cloudy"),
+  createWeatherCardEl([{ name: "class", value: "card" }], "div", "Rainy"),
+];
+
+createWeatherCard(weatherCardEls);
+
+// api calls?
+
+// link to forecast
+// updateInterface
+
+// const todayDate = () => {};
+
+// date helpers
+
+// link to forecasts
+
+// const forecastBg = (weather) => {
+// const body = document.getElementById("forecastBody");
+
+// // switch (true) {
+// // case weather.includes("rain"):
+// //   case "Clear":
+// //     return "#FFE873";
+// //   case "Clouds":
+// //     return "#C7C7CC";
+// //   case "Drizzle":
+// //   case "Rain":
+// //   case "Thunderstorm":
+// //     return "#4B4B4C";
+// //   case "Snow":
+// //     return "#D4F2FF";
+// //   case "Mist":
+// //   case "Smoke":
+// //   case "Haze":
+// //   case "Dust":
+// //   case "Fog":
+// //   case "Sand":
+// //   case "Ash":
+// //     return "#A7A9AC";
+// //   case "Squall":
+// //   case "Tornado":
+// //     return "#4B4B4C";
+// //   default:
+// //     return "#FFFFFF";
+
+// // default: --bgcolor
+
+// // // .style.setProperty
+// // }
+
+// // }
