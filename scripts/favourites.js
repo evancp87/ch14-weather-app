e31554507c1fede5adf68f446a4ad21bc26e5e63
Evan Parker <65721884+evancp87@users.noreply.github.com>
2023-05-12 21:03:55 +0100

    util functions added and partialy implemented into favorites

--- scripts/favourites.js
+++ scripts/favourites.js
@@ -2,7 +2,17 @@
 // import { key } from "./index";
 
 import apiKey from "../apiKey.js";
-// import createWeatherCard from "./helpers.js";
+import {
+  createWeatherCardElement,
+  createWeatherCard,
+  appendCardEl,
+  appendCardElements,
+  apiCall,
+  linkToForecast,
+  // removeFavorite,
+  attachRemoveListener,
+  attachCardClickListener,
+} from "./utils.js";
 
 const app = document.getElementById("root");
 
@@ -13,108 +23,135 @@ export const getFavorites = async () => {
     const citiesArray = JSON.parse(citiesString);
     const currentFavoritesLength = citiesArray.length;
 
-    const favoritesSection = document.createElement("section");
-    favoritesSection.setAttribute("class", "favorites");
-    const cardHeading = document.createElement("h2");
-    cardHeading.classList.add("card__heading");
-    cardHeading.innerText = "Favorites";
-    favoritesSection.appendChild(cardHeading);
+    const favoritesSection = createWeatherCardElement(
+      [{ name: "class", value: "favorites" }],
+      "section"
+    );
+    const headingArr = [
+      // createWeatherCardElement([{ name: "class", value: "favorites" }], "section"),
+      createWeatherCardElement(
+        [{ name: "class", value: "card__heading" }],
+        "h2",
+        "Favorites"
+      ),
+    ];
+
+    appendCardEl(headingArr, favoritesSection);
+
     app.appendChild(favoritesSection);
 
     // loop over favorites array and for each get the key and make an api call the item from localstorage
     for (let city of citiesArray) {
       console.log("the city is:", city);
-      const { data } = await axios.get(
-        `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`
+
+      const weatherApiData = await apiCall(city, apiKey);
+      createWeatherCard(weatherApiData, favoritesSection);
+      // if (weatherApiData) {
+      //   const card = createWeatherCardElement(
+      //     [
+      //       { name: "id", value: "`${city}`" },
+      //       { name: "class", value: "favorite card dropzone" },
+      //     ],
+      //     "div"
+      //   );
+      //   const cardText = createWeatherCardElement(
+      //     [{ name: "class", value: "card__text" }],
+      //     "div"
+      //   );
+
+      //   const weatherCardElArr = [
+      //     createWeatherCardElement(
+      //       [{ name: "class", value: "card__heading" }],
+      //       "h2",
+      //       weatherApiData.name
+      //     ),
+      //     createWeatherCardElement(
+      //       [{ name: "class", value: "card__current-weather" }],
+      //       "p",
+      //       weatherApiData.description
+      //     ),
+      //   ];
+      //   appendCardElements(weatherCardElArr, cardText);
+
+      //   // **
+      //   const celsius = Math.round(weatherApiData.temp - 273.15) + "°C";
+      //   const iconsContainer = createWeatherCardElement(
+      //     [{ name: "class", value: "card__icons" }],
+      //     "div"
+      //   );
+
+      //   const weatherData = [
+      //     createWeatherCardElement(
+      //       [
+      //         {
+      //           name: "src",
+      //           value: `https://openweathermap.org/img/w/${weatherApiData.icon}.png`,
+      //         },
+      //         { name: "class", value: "weather-icon" },
+      //         { name: "alt", value: `${weatherApiData.name} weather icon` },
+      //       ],
+      //       "img"
+      //     ),
+
+      //     createWeatherCardElement(
+      //       [{ name: "class", value: "card__temp" }],
+      //       "p",
+      //       celsius
+      //     ),
+      //   ];
+
+      //   appendCardElements(weatherData, iconsContainer);
+
+      //   const cardContainer = createWeatherCardElement(
+      //     [{ name: "class", value: "card__container" }],
+      //     "div"
+      //   );
+
+      //   // card.appendChild(cardContainer);
+      //   appendCardElements([cardText, iconsContainer], cardContainer);
+      //   // appendCardEl(iconsContainer, cardContainer);
+      //   appendCardEl(cardContainer, card);
+
+      // cardContainer.append(cardText, iconsContainer);
+
+      // appendCardElements(cardContainer, favoritesSection);
+
+      const removeButton = createWeatherCardElement(
+        [
+          { name: "id", value: "removeBtn" },
+          { name: "class", value: "remove-btn" },
+          { name: "src", value: "../images/minus.png" },
+          { name: "alt", value: "minus icon" },
+        ],
+        "img"
       );
-      const {
-        name,
-        weather: [{ main, description, icon }],
-        main: { temp, humidity },
-        coord: { lon, lat },
-        wind: { speed },
-        visibility,
-      } = data;
 
-      const card = document.createElement("div");
-      card.setAttribute("id", `${city}`);
-      // card.setAttribute("class", "card");
-      card.setAttribute("class", "favorite card dropzone");
-      card.setAttribute("class", "favorite card dropzone");
+      // appendCardEl(removeButton, iconsContainer);
+      // appendCardEl(card, favoritesSection);
 
-      const cardText = document.createElement("div");
-      cardText.setAttribute("class", "card__text");
+      // removeFavorite(e, card, city);
 
-      const cityName = document.createElement("h2");
-      cityName.textContent = name;
+      // removeButton.addEventListener("click", removeFavorite);
 
-      const cardContainer = document.createElement("div");
-      cardContainer.setAttribute("class", "card__container");
+      attachRemoveListener(removeButton, card, city);
+      attachCardClickListener(card, weatherApiData);
 
-      const currWeather = document.createElement("p");
-      currWeather.textContent = description;
-
-      const iconsContainer = document.createElement("div");
-      iconsContainer.setAttribute("class", "card__icons");
-
-      const tempData = document.createElement("p");
-      const celsius = Math.round(temp - 273.15) + "°C";
-      tempData.textContent = celsius;
-      const iconImg = document.createElement("img");
-      iconImg.setAttribute(
-        "src",
-        `https://openweathermap.org/img/w/${icon}.png`
-      );
-      iconImg.setAttribute("class", `weather-icon`);
-      iconImg.setAttribute("alt", `${name} weather icon`);
-      cardText.appendChild(cityName);
-      // card__text.appendChild(form);
-      cardText.appendChild(currWeather);
-      card.appendChild(cardContainer);
-
-      cardContainer.appendChild(cardText);
-      // card.appendChild(weatherIcon);
-      iconsContainer.appendChild(tempData);
-      iconsContainer.appendChild(iconImg);
-      cardContainer.appendChild(iconsContainer);
-
-      // removes the plus button
-
-      const removeButton = document.createElement("img");
-      removeButton.setAttribute("id", "removeBtn");
-      removeButton.setAttribute("class", "remove-btn");
-      removeButton.setAttribute("src", "../images/minus.png");
-      removeButton.setAttribute("alt", "minus icon");
-
-      iconsContainer.appendChild(removeButton);
-      favoritesSection.appendChild(card);
-      // app.appendChild(favoritesSection);
-
-      const removeFavorite = (e) => {
-        e.stopPropagation();
-        // save variable of item
-
-        const itemToDelete = city;
-        const citiesString = localStorage.getItem("cities");
-        let citiesArray = JSON.parse(citiesString);
-
-        // Remove the selected city from the array
-        citiesArray = citiesArray.filter(
-          (cityName) => cityName !== itemToDelete
-        );
-
-        // Update the "cities" key in the localStorage
-        localStorage.setItem("cities", JSON.stringify(citiesArray));
-
-        console.log(itemToDelete, "was deleted");
-        card.remove();
-      };
-      removeButton.addEventListener("click", removeFavorite);
-
-      card.addEventListener("click", () => {
-        // const cityName = cityName.textContent;
-        window.location.href = `forecast.html?city=${cityName.textContent}&lon=${lon}&lat=${lat}&description=${description}&temp=${temp}&humidity=${humidity}&speed=${speed}&visibility=${visibility}&icon=${icon}`;
-      });
+      // card.addEventListener("click", () => {
+      //   // const cityName = cityName.textContent;
+      //   linkToForecast(
+      //     card,
+      //     weatherApiData.name,
+      //     weatherApiData.lon,
+      //     weatherApiData.lat,
+      //     weatherApiData.description,
+      //     weatherApiData.temp,
+      //     weatherApiData.humidity,
+      //     weatherApiData.speed,
+      //     weatherApiData.visibility,
+      //     weatherApiData.icon
+      //   );
+      //   // window.location.href = `forecast.html?city=${name}&lon=${lon}&lat=${lat}&description=${description}&temp=${temp}&humidity=${humidity}&speed=${speed}&visibility=${visibility}&icon=${icon}`;
+      // });
     }
     // app.appendChild(favoritesSection);
 
@@ -137,6 +174,7 @@ export const getFavorites = async () => {
       } else {
         console.log("element not found");
       }
+      // }
     }
   } catch (error) {
     console.error("error", error);
---
+++ scripts/utils.js
@@ -0,0 +1,242 @@
+import apiKey from "../apiKey.js";
+
+export const createWeatherCardElement = (
+  attributes = [],
+  tag,
+  text = "",
+  children = []
+) => {
+  const element = document.createElement(tag);
+
+  if (text !== "") {
+    const _text = document.createTextNode(text);
+    element.append(_text);
+  }
+
+  if (attributes) {
+    attributes.forEach(({ name, value }) => {
+      element.setAttribute(name, value);
+    });
+
+    if (children) {
+      children.forEach((child) => {
+        element.appendChild(child);
+      });
+    }
+  }
+  // document.getElementById(id).append(element);
+  return element;
+};
+
+export const appendCardEl = (child, domRef) => {
+  // const parentEl = document.querySelector(`.${domRef[value]}`);
+  domRef.append(child);
+};
+
+export const appendCardElements = (elements, domRef) => {
+  elements.forEach((element) => {
+    domRef.appendChild(element);
+  });
+};
+
+export const apiCall = async (city, apiKey) => {
+  const { data } = await axios.get(
+    `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`
+  );
+  const {
+    name,
+    weather: [{ main, description, icon }],
+    main: { temp, humidity },
+    coord: { lon, lat },
+    wind: { speed },
+    visibility,
+  } = data;
+
+  return {
+    name,
+    main,
+    description,
+    icon,
+    temp,
+    humidity,
+    lon,
+    lat,
+    speed,
+    visibility,
+  };
+};
+
+export const linkToForecast = (
+  elem,
+  name,
+  lon,
+  lat,
+  description,
+  temp,
+  humidity,
+  speed,
+  visibility,
+  icon
+) => {
+  elem.addEventListener("click", () => {
+    // const cityName = cityName.textContent;
+    window.location.href = `forecast.html?city=${name}&lon=${lon}&lat=${lat}&description=${description}&temp=${temp}&humidity=${humidity}&speed=${speed}&visibility=${visibility}&icon=${icon}`;
+  });
+};
+
+export const createWeatherCard = (weatherApiData, favoritesSection) => {
+  if (weatherApiData) {
+    const card = createWeatherCardElement(
+      [
+        { name: "id", value: "`${city}`" },
+        { name: "class", value: "favorite card dropzone" },
+      ],
+      "div"
+    );
+
+    const cardText = createWeatherCardElement(
+      [{ name: "class", value: "card__text" }],
+      "div"
+    );
+
+    const weatherCardElArr = [
+      createWeatherCardElement(
+        [{ name: "class", value: "card__heading" }],
+        "h2",
+        weatherApiData.name
+      ),
+      createWeatherCardElement(
+        [{ name: "class", value: "card__current-weather" }],
+        "p",
+        weatherApiData.description
+      ),
+    ];
+    appendCardElements(weatherCardElArr, cardText);
+
+    // **
+    const celsius = Math.round(weatherApiData.temp - 273.15) + "°C";
+    const iconsContainer = createWeatherCardElement(
+      [{ name: "class", value: "card__icons" }],
+      "div"
+    );
+
+    const weatherData = [
+      createWeatherCardElement(
+        [
+          {
+            name: "src",
+            value: `https://openweathermap.org/img/w/${weatherApiData.icon}.png`,
+          },
+          { name: "class", value: "weather-icon" },
+          { name: "alt", value: `${weatherApiData.name} weather icon` },
+        ],
+        "img"
+      ),
+
+      createWeatherCardElement(
+        [{ name: "class", value: "card__temp" }],
+        "p",
+        celsius
+      ),
+    ];
+
+    appendCardElements(weatherData, iconsContainer);
+
+    const cardContainer = createWeatherCardElement(
+      [{ name: "class", value: "card__container" }],
+      "div"
+    );
+
+    // card.appendChild(cardContainer);
+    appendCardElements([cardText, iconsContainer], cardContainer);
+    // appendCardEl(iconsContainer, cardContainer);
+    appendCardEl(cardContainer, card);
+    appendCardEl(card, favoritesSection);
+  }
+};
+
+export const removeFavorite = (e, card, city) => {
+  e.stopPropagation();
+  // save variable of item
+
+  const itemToDelete = city;
+  const citiesString = localStorage.getItem("cities");
+  let citiesArray = JSON.parse(citiesString);
+
+  // Remove the selected city from the array
+  citiesArray = citiesArray.filter((cityName) => cityName !== itemToDelete);
+
+  // Update the "cities" key in the localStorage
+  localStorage.setItem("cities", JSON.stringify(citiesArray));
+
+  console.log(itemToDelete, "was deleted");
+  card.remove();
+};
+
+export const attachRemoveListener = (removeButton, card, city) => {
+  appendCardEl(removeButton, iconsContainer);
+  removeButton.addEventListener("click", (e) => {
+    removeFavorite(e, card, city);
+  });
+};
+
+export const attachCardClickListener = (card, weatherApiData) => {
+  card.addEventListener("click", () => {
+    linkToForecast(
+      card,
+      weatherApiData.name,
+      weatherApiData.lon,
+      weatherApiData.lat,
+      weatherApiData.description,
+      weatherApiData.temp,
+      weatherApiData.humidity,
+      weatherApiData.speed,
+      weatherApiData.visibility,
+      weatherApiData.icon
+    );
+  });
+};
+
+// updateInterface
+
+// const todayDate = () => {};
+
+// date helpers
+
+// link to forecasts
+
+// const forecastDynamicBackground = (weather) => {
+// const body = document.getElementById("forecastBody");
+
+// // switch (true) {
+// // case weather.includes("rain"):
+// //   case "Clear":
+// //     return "#FFE873";
+// //   case "Clouds":
+// //     return "#C7C7CC";
+// //   case "Drizzle":
+// //   case "Rain":
+// //   case "Thunderstorm":
+// //     return "#4B4B4C";
+// //   case "Snow":
+// //     return "#D4F2FF";
+// //   case "Mist":
+// //   case "Smoke":
+// //   case "Haze":
+// //   case "Dust":
+// //   case "Fog":
+// //   case "Sand":
+// //   case "Ash":
+// //     return "#A7A9AC";
+// //   case "Squall":
+// //   case "Tornado":
+// //     return "#4B4B4C";
+// //   default:
+// //     return "#FFFFFF";
+
+// // default: --bgcolor
+
+// // // .style.setProperty
+// // }
+
+// // }
