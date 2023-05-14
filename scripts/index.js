c4aaf21fb0b08e29bebeb4b08d0e02000cdb9008
Evan Parker <65721884+evancp87@users.noreply.github.com>
2023-05-14 10:24:39 +0100

    removed drag api code

--- scripts/index.js
+++ scripts/index.js
@@ -1,13 +1,11 @@
 import { getCurrLocation } from "./geolocation.js";
 import { getFavorites } from "./favourites.js";
-import { loadDraggableItems } from "./drag.js";
 
 console.log("hello world");
 
 const onLoad = () => {
   getCurrLocation();
   getFavorites();
-  loadDraggableItems();
 };
 
 window.addEventListener("load", onLoad);
