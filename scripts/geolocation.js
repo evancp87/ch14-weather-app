8ceef50e35dc382a3938565be23034273a68ec4c
Evan Parker <65721884+evancp87@users.noreply.github.com>
2023-05-14 10:19:53 +0100

    refactor js, added greeting and skeleton loading

--- scripts/geolocation.js
+++ scripts/geolocation.js
@@ -1,8 +1,16 @@
 import apiKey from "../apiKey.js";
 import {
   createWeatherCard,
+  createWeatherCardElement,
   attachCardClickListener,
+  appendCardEl,
+  appendCardElements,
   geolocationApiCall,
+  appendElementsToSearch,
+  appendChildEl,
+  getGreeting,
+  getCurrentTime,
+  skeletonLoading,
 } from "./utils.js";
 
 const app = document.getElementById("root");
@@ -21,30 +29,75 @@ const options = {
 };
 
 navigator.geolocation.getCurrentPosition(
+  (position) => {
+    skeletonLoading();
+    successCallback(position);
+  },
   successCallback,
   errorCallback,
   options
 );
 
 export const getCurrLocation = () => {
-  const container = document.createElement("section");
-  container.classList.add("location-container");
-  const cardHeading = document.createElement("h2");
-  cardHeading.classList.add("card__heading");
-  cardHeading.innerText = "Current Location";
-  container.appendChild(cardHeading);
+  const container = createWeatherCardElement(
+    [{ name: "class", value: "location-container" }],
+    "section"
+  );
 
-  const searchItem = `<div class="landing-page__search-blurb">
-                <p>Good morning! Discover the weather</p>
-                <div class="landing-page__search-input">
-                    <a href="./search.html">
-                        <img src="./images/research.png" alt="search icon" class="search-icon">
-                    </a>
-                </div>
+  // const container = document.createElement("section");
+  // container.classList.add("location-container");
 
-            </div>`;
+  // const searchItem = `<div class="landing-page__search-blurb">
+  //               <p>Good morning! Discover the weather</p>
+  //               <div class="landing-page__search-input">
+  //                   <a href="./search.html">
+  //                       <img src="./images/research.png" alt="search icon" class="search-icon">
+  //                   </a>
+  //               </div>
 
-  container.innerHTML = searchItem;
+  //           </div>`;
+
+  const textContainer = createWeatherCardElement(
+    [{ name: "class", value: "landing-page__search-blurb" }],
+    "div"
+  );
+
+  const greeting = createWeatherCardElement(
+    [{ name: "class", value: "landing-page__greeting" }],
+    "p",
+    `Good ${getGreeting(getCurrentTime())}! Discover the weather`
+  );
+  const searchInput = createWeatherCardElement(
+    [{ name: "class", value: "landing-page__search-input" }],
+    "div"
+  );
+
+  const linkToSearchInput = createWeatherCardElement(
+    [{ name: "href", value: "./search.html" }],
+    "a"
+  );
+  const searchImg = createWeatherCardElement(
+    [
+      { name: "src", value: "./images/research.png" },
+      { name: "alt", value: "search icon" },
+      { name: "class", value: "search-icon" },
+    ],
+    "img"
+  );
+  // container.innerHTML = searchItem;
+  const greetingEls = [greeting, searchInput, searchImg, linkToSearchInput];
+  const searchEls = [searchImg, linkToSearchInput];
+  appendCardEl(linkToSearchInput, searchImg);
+  appendElementsToSearch(greetingEls, textContainer);
+  appendElementsToSearch(searchEls, searchInput);
+
+  const locationHeader = createWeatherCardElement(
+    [{ name: "class", value: "location__header" }],
+    "h2",
+    "Current Location"
+  );
+
+  appendCardElements([textContainer, locationHeader], container);
 
   return navigator.geolocation.getCurrentPosition(async (position) => {
     try {
@@ -52,6 +105,10 @@ export const getCurrLocation = () => {
 
       const weatherData = await geolocationApiCall(latitude, longitude, apiKey);
 
+      const loadingContainer = document.querySelector(".card.skeleton-card");
+      if (loadingContainer) {
+        loadingContainer.remove();
+      }
       // Display the weather information on the webpage
 
       const { card } = createWeatherCard(weatherData, container);
